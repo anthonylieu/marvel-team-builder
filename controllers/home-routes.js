740dc9ef916648d58c25dc3651fdb0e576b3ebd4
anthonylieu <38957648+anthonylieu@users.noreply.github.com>
2023-04-18 15:08:45 -0400

    home-routes.js now works with env, had to install dotenv

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,31 @@
-const router = require("express").Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
 
-router.get("/", async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
-  res.render("homepage");
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'anthonylieu',
+  password: 'password',
+  database: 'marvelteam_db'
+});
+
+// Get all characters
+router.get('/characters', (req, res) => {
+  const sql = 'SELECT * FROM Characters';
+  connection.query(sql, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+// Get a character by ID
+router.get('/characters/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = `SELECT * FROM Characters WHERE id = ${id}`;
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    res.json(result[0]);
+  });
 });
 
 module.exports = router;
