67d03c0192815062d78074d6c5af5bf784cc63c8
Sean Noway <sean.m.holian@gmail.com>
2023-04-19 14:59:36 -0400

    added rendering to js

--- controllers/character-sheet.js
+++ controllers/character-sheet.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const {Character} = require("../models");
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
-    res.render("character-sheet")
+// router.get('/', (req, res) => {
+//     res.render("character-sheet")
+//   });
+
+  router.get("/", async(req, res) => {
+    const characterData = await Character.findAll().catch((err) => { 
+      res.json(err);
+    });
+    const characters = characterData.map((character) => character.get({ plain: true }));
+    console.log(characters);
+    // Replace 'characters' with actual data you want to pass to the template
+    res.render("character-sheet", { characters });
   });
+
   module.exports = router;
