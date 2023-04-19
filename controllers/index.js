e4e491d067095f48bec07a2d44057cec95daece8
Sean Noway <sean.m.holian@gmail.com>
2023-04-19 13:32:07 -0400

    added body tags to handelbars

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
-
+const characterSheet = require("./character-sheet");
+router.use("/character", characterSheet);
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 
