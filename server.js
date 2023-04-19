bc621c495ad969a51be5649782b47a4cfa18daa9
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-04-19 13:35:44 -0400

    corrected handlebars to follow actual functionality

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
 const express = require("express");
 const app = express();
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 const hbs = exphbs.create({});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {
     maxAge: 300000,
     httpOnly: true,
     secure: false,
-    sameSite: 'strict',
+    sameSite: "strict",
   },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 app.use(session(sess));
 // Import routes
-const homeRoutes = require('./controllers/home-routes.js');
+const homeRoutes = require("./controllers/home-routes.js");
 // Set up routes
-app.use('/', homeRoutes);
+app.use("/", homeRoutes);
 app.use(routes);
 // Start the server
 sequelize.sync({ force: false }).then(() => {
