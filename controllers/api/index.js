367fd7637c778ea2176a07aa1263064c2d9b6b8d
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-04-20 21:31:51 -0400

    fixed missing stat

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const teamRoute = require("./team");
+const userRoutes = require("./user-routes");
 
-const userRoutes = require('./user-routes');
-
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
+// router.use("/teams", teamRoute);
 
 module.exports = router;
