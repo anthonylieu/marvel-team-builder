77bf4c090e8fc4742d05b922d3c5df33ede751be
Sean Noway <sean.m.holian@gmail.com>
2023-04-25 18:35:02 -0400

    hosuekeeping

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -63,26 +63,4 @@ router.post("/signup", async (req, res) => {
 });
 
 // Render the character-sheet
-
 module.exports = router;
-
-// router.post('/', (req, res)=>{
-//   User.create({
-//       username: req.body.username,
-//       email: req.body.email,
-//       password: req.body.password
-//   })
-//   .then(dbUserData =>{
-//       req.session.save(()=>{
-//           req.session.user_id = dbUserData.id;
-//           req.session.username = dbUserData.username;
-//           req.session.loggedIn = true;
-
-//           res.json(dbUserData);
-//       });
-//   })
-//   .catch(err=>{
-//       console.log(err);
-//       res.status(500).json(err);
-//   });
-// });
